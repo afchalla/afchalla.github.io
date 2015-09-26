f6a285befc542c92394172156c6cfb3eb9b5542a
cfakhruddin <cfakhruddin@zynga.com>
2015-09-26 15:41:47 +0530

    sudoku.js - auto determination before backtracking

--- sudoku-solver/js/sudoku.js
+++ sudoku-solver/js/sudoku.js
@@ -25,6 +25,138 @@ var sudokuSolver = {
 		return success;
 	},
 
+	tryFillingInSimple: function() {
+		var trySuccess;
+		do {
+			trySuccess = false;
+			for(var row = 0; row < this.size; row++) {
+				for(var col = 0; col < this.size; col++) {
+
+					if(this.marker[row][col] == 1) {
+						continue;
+					}
+
+					var probable = 0;
+					var value = 0;
+					for(var tryInput = 1; tryInput <= this.size; tryInput++) {
+						if(this.canFitInValue(row, col, tryInput)) {
+							probable++;
+							value = tryInput;
+						}
+					}
+					if(probable == 1) {
+						this.input[row][col] = value;
+						this.marker[row][col] = 1;
+						trySuccess = true;
+					}
+				}
+			}
+		} while(trySuccess);
+	},
+
+	tryFillingInElimination: function() {
+
+		var colorer = [];
+		var counter = 0;
+
+		for (var i = 0; i < this.size; i++) {
+			colorer.push(new Array(this.size));
+		}
+
+		do {
+
+			counter = 0;
+			for(var tryInput = 1; tryInput <= this.size; tryInput++) {
+
+				var colorSuccess = false;
+				do {
+
+					colorSuccess = false;
+
+					for(var row = 0; row < this.size; row++) {
+						for(var col = 0; col < this.size; col++) {
+							colorer[row][col] = 0;
+						}
+					}
+
+					for(var row = 0; row < this.size; row++) {
+						for(var col = 0; col < this.size; col++) {
+							if(this.input[row][col] == tryInput) {
+
+								var boxRow = parseInt(row/this.root)*this.root;
+								var boxCol = parseInt(col/this.root)*this.root;
+
+								for(var i = 0; i < this.size; i++) {
+									colorer[row][i] = 1;
+									colorer[i][col] = 1;
+									colorer[boxRow][boxCol] = 1;
+
+									boxCol++;
+									if(boxCol % this.root == 0) {
+										boxCol = parseInt(col/this.root)*this.root;
+										boxRow++;
+									}
+								}
+
+								break;
+							}
+						}
+					}
+
+					for(var row = 0; row < this.size; row++) {
+						for(var col = 0; col < this.size; col++) {
+							if(colorer[row][col] == 0 && this.input[row][col] == 0) {
+
+								var onlyProbable = true;
+								for(var rowCheck = 0 ; rowCheck < this.size; rowCheck++) {
+									if(col == rowCheck) {
+										continue;
+									}
+
+									if(colorer[row][rowCheck] != 0 || this.input[row][rowCheck] != 0) {
+										continue;
+									}
+
+									onlyProbable = false;
+									break;
+								}
+
+								if(!onlyProbable) {
+									onlyProbable = true;
+									for(var colCheck = 0 ; colCheck < this.size; colCheck++) {
+										if(row == colCheck) {
+											continue;
+										}
+
+										if(colorer[colCheck][col] != 0 || this.input[colCheck][col] != 0) {
+											continue;
+										}
+
+										onlyProbable = false;
+										break;
+									}
+								}
+
+								if(onlyProbable) {
+									this.input[row][col] = tryInput;
+									this.marker[row][col] = 1;
+									colorSuccess = true;
+									counter++;
+									break;
+								}
+							}
+						}
+						if(colorSuccess) {
+							break;
+						}
+					}
+
+				} while(colorSuccess);
+
+			}
+		} while (counter > 0);
+
+	},
 
 	checkInputValidity: function() {
 		var result = true;
@@ -119,6 +251,9 @@ var sudokuSolver = {
 				row++;
 			}
 		}
+
+		this.tryFillingInSimple();
+		this.tryFillingInElimination();
 
 		if(!this.checkInputValidity() || !this.solveSudoku(0,0)) {
 			return -2;
